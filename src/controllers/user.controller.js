00f38f31a1d9f6a66063255019f5b01069e5dc56
arjun <arjunsharma3368@gmail.com>
2025-12-02 23:14:33 +0530

    add endpoint refresh-token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,24 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken"
+
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
 
 const registerUser=asyncHandler(async (req,res)=>
    // get data from frontent about user
@@ -83,4 +101,142 @@ const registerUser=asyncHandler(async (req,res)=>
    
 )
 
-export {registerUser}
+const loginUser=asyncHandler(async(req,res)=>{
+    //req data from body
+    // username or  email
+    //check is the user registered
+    //check password then
+    // access and refresh token
+    // send cookie
+
+    //just keep in mind only then only send data with forms when u are using any middleware in between beacuse it share both text and file
+       console.log(req.body);
+
+    const {username,email,password}=req.body;
+
+    console.log(req.body);
+
+    if(!username && !email){
+       throw new ApiError(400,"username and email is required")
+    }
+
+    const user=await User.findOne({
+      $or:[{username},{email}]
+    })
+    
+
+    if(!user){
+      throw new ApiError(404,"user is not registered")
+    }
+
+    const isPasswordValid=user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+       throw new ApiError(401,"password is wrong")
+    }
+
+      const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+
+      const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)//accesstoken name cookie we are adding in aur res using cookie parser
+    .cookie("refreshToken", refreshToken, options)//refreshtoken name cookie we are adding in aur res using cookie parser
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+
+})
+
+const logoutUser=asyncHandler(async(req,res)=>{
+  await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+const refreshAccessToken=asyncHandler(async(req, res)=>{
+    const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
+
+    if(!incomingRefreshToken){
+      throw new ApiError(401,"unotharized request")
+    }
+
+    try {
+      const decodedToken=jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+        
+      )
+  
+      const user=await User.findById(decodedToken?._id);
+  
+      if(!user){
+        throw new ApiError(401,"invalid token")
+      }
+  
+      if(incomingRefreshToken!==user.refreshToken){
+        throw new ApiError(401,"refresh token is expired or used")
+      }
+      
+      const options = {
+          httpOnly: true,
+          secure: true
+      }
+  
+      const {accessToken,newRefreshToken}=await generateAccessAndRefereshTokens(user._id)
+  
+      return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+          new ApiResponse(
+              200, 
+              {
+                 accessToken, refreshToken:newRefreshToken
+              },
+              "User logged In Successfully"
+          )
+      )
+    } catch (error) {
+      throw new ApiError(401,error?.message||"invalid token")
+    }
+
+})
+
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router=Router();
 
 router.route("/register").post(
-    upload.fields([
+    upload.fields([  // the use of multer lead to a extension which lead to add files in cloudinary with disturbing express beacuse it cant handle it 
     {
         name:"avatar",
-        maxCount:1
+        maxCount:1          // thats the reason why field like avatar and coverImgae is handle by it
     },
     {
         name:"coverImage",
@@ -16,5 +17,12 @@ router.route("/register").post(
     }
 ]),
     registerUser)
+
+
+    router.route("/login").post(loginUser);
+
+    //secured routes
+    router.route("/logout").post(verifyJWT,logoutUser)
+    router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
