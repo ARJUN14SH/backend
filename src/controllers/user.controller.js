8dac44e277166a7ab1ab684295d1f7c47e905992
arjun <arjunsharma3368@gmail.com>
2025-12-03 02:17:44 +0530

    add user account management features including password change, user details update, avatar and cover image upload, user channel profile retrieval, and watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -237,6 +238,278 @@ const refreshAccessToken=asyncHandler(async(req, res)=>{
 
 })
 
+const changeCurrentPassword=asyncHandler(async(req,res)=>{
+  const {oldPassword,newPassword}=req.body;
+
+  const user=await User.findById(req.user?._id);
+  const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+
+  if(!isPasswordCorrect){
+    throw new ApiError(400,"invalid old password")
+  }
+
+  user.password=newPassword;
+  await user.save({validateBeforeSave:false})
+
+  return res.status(200).json(
+    new ApiResponse(200,{},"password changed successfully")
+  )
+})
+
+const getCurrentUser=asyncHandler(async(req,res)=>{
+
+  return res.status(200)
+  .json( new ApiResponse(200,req.user,"currect user fetched successfully"))
+
+})
+
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+  const {fullName,email}=req.body;
+
+    if(!fullName && !email){
+      throw new ApiError(400,"all field are required")
+    }
+
+    const user=await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+    $set:{
+         fullName,
+         email
+         }
+      },
+      {
+        new:true
+      }
+    ).select("-password")
 
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+    return res.status(200)
+    .json(
+     new ApiResponse(200,user,"user detail updated successfully")
+    )
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Cover image updated successfully")
+    )
+})
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+
+  const {username}=req.params;
+
+    if(!username?.trim()){
+      throw new ApiError(400,"username is missing")
+    }
+
+    const channel=await User.aggregate([
+      {
+        $match:{
+           username: username?.toLowerCase()
+        }
+      },
+      /*
+      this is what happing in lookupthat there is a fiel of subscriber which is as that have coolection of that of 
+      different user for same channel
+      
+      {
+  "_id": "userB",
+  "username": "rohan",
+
+  "subscriber": [
+    { "subscriber": "userA", "channel": "userB" },
+    { "subscriber": "userC", "channel": "userB" },
+    { "subscriber": "userD", "channel": "userB" }
+  ]
+        }
+   */
+      {
+        $lookup:{
+          from:"subscriptions",
+          localField:"_id",
+          foreignField:"channel",// what here the foreign field do that is says to subscription model that every user with this channel field should be here
+          as:"subscriber"
+        }
+      },
+      /*here the thing are just swapped that foreignfield is subscriber which points that this 
+      subscriber subscribed to whi
+      which channel  
+      
+      the resson that there is the relation only betwwen channel and subscrible is (from:"subscription")which is a
+      model on those two input field
+      
+      */
+      {
+          $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+      },
+      {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+            }
+        }
+    ])
+
+
+    if(!channel?.length){
+      throw new ApiError(404,"channel does not exist");
+    }
+
+     return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
+const getWatchHistory=asyncHandler(async(req,res)=>{
+       const user=await User.aggregate([
+        {
+          $match:{
+                 _id: new mongoose.Types.ObjectId(req.user._id)//this way we write in pipelines
+
+          },
+           $lookup:{
+              from:"videos",
+              localField:"watchHistory",
+              foreignField:"_id",
+              as:"watchHistory",
+              pipeline:[
+                {
+                  $lookup:{
+                    from:"users",
+                    localField:"owner",
+                    foreignField:"_id",
+                    as:"owner",
+                    pipeline:[
+                      {
+                        $project:{
+                          fullName:1,
+                          username:1,
+                          avatar:1
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                       ]
+            }
+        }
+    ])
+
+     return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+
+  })
+
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,
+  updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -24,5 +24,14 @@ router.route("/register").post(
     //secured routes
     router.route("/logout").post(verifyJWT,logoutUser)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+    router.route("/currect-user").post(verifyJWT,getCurrentUser)
+    router.route("/update_account").post(verifyJWT,updateAccountDetails)
+    router.route("/avatar").post(verifyJWT,upload.single(avatar),updateUserAvatar)
+    router.route("/coverImage").post(verifyJWT,upload.single(coverImage),updateUserCoverImage)
+    router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+    router.route("/history").get(verifyJWT,getWatchHistory)
+
+
 
 export default router
